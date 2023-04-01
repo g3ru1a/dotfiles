0996f0b92030c0ae42d5f4826947b2acbe3e8722
G3ru1a <egidiufarcas@maze.ws>
2023-04-01 20:13:16 +0100

    added more verbosity to the post script

--- templates/gerula/post-setup.sh
+++ templates/gerula/post-setup.sh
@@ -2,28 +2,40 @@
 
 username=$1
 dir="$PWD/templates/gerula"
-echo "$PWD"
 
 echo "Configuring the environment..."
 
+echo "Copying bspwm config"
 cp -r "$dir"/.config/bspwm ~/.config/bspwm
+echo "Copying dunst config"
 cp -r "$dir"/.config/dunst ~/.config/dunst
+echo "Copying kitty config"
 cp -r "$dir"/.config/kitty ~/.config/kitty
+echo "Copying picom config"
 cp -r "$dir"/.config/picom ~/.config/picom
+echo "Copying polybar config"
 cp -r "$dir"/.config/polybar ~/.config/polybar
+echo "Copying rofi config"
 cp -r "$dir"/.config/rofi ~/.config/rofi
+echo "Copying sxhkd config"
 cp -r "$dir"/.config/sxhkd ~/.config/sxhkd
 
+echo "Copying scripts, styles and fonts"
 cp -r "$dir"/scripts ~/
 cp -r "$dir"/styling ~/
 cp -r "$dir"/.fonts ~/
+echo "Copying shell profile"
 cp "$dir"/.profile ~/
 cp "$dir"/.zshenv ~/
 cp "$dir"/.zshrc ~/
 
+echo "Updating profile name"
 sed -i "s/<user>/$username/" ~/.profile
 sed -i "s/<user>/$username/" ~/.zshenv
 
+echo "Updating X11 xkb symbols"
 sudo rm -rf /usr/share/X11/xkb/symbols/pc
 sudo cp "$dir"/scripts/usr-share-X11-xkb-symbols-pc /usr/share/X11/xkb/symbols/
 sudo mv /usr/share/X11/xkb/symbols/usr-share-X11-xkb-symbols-pc /usr/share/X11/xkb/symbols/pc
+
+echo "Setup Complete! Reboot and login in bspwm."
